37529a0421a435d077c9d92cd8b15cfb895f3fbc
giacomo-spadoni <giaky.spado@gmail.com>
2024-04-19 10:05:53 +0200

    added select options

--- js/main.js
+++ js/main.js
@@ -3,22 +3,17 @@ const prezzo = document.getElementById("price");
 
 prezzo.addEventListener('click', function () {
     let input = document.getElementById("destination");
-    let inputValue = input.value;
     let input2 = document.getElementById("age");
-    let input2Value = input2.value;
 
+    let age = parseInt(input2.value);
 
-    if (input2.value > 65) {
+    if (age > 65) {
         costo = 0.21 * 0.6;
+    } else if (age < 18) {
+        costo = 0.21 * 0.8;
     } else {
-        if (input2.value < 18) {
-            costo = 0.21 * 0.8
-        } else {
-            costo = 0.21
-        }
+        costo = 0.21
     }
-
-
 
     let costoTotale = costo * input.value
     document.getElementById("costo-totale").innerHTML = costoTotale.toFixed(2) + "€"
